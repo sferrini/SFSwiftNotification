33a2a9676eade744241a40b67447ad3f2d25b2de
Simone Ferrini <sferrini@hotmail.it>
2014-07-13 15:38:11 +0100

    Initial Commit

---
+++ SFSwiftNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFSwiftNotification
+//
+//  Created by Simone Ferrini on 13/07/14.
+//  Copyright (c) 2014 sferrini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
