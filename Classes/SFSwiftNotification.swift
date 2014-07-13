18deb1c516c020ec9bd0cd281826e500885fe44d
Simone Ferrini <sferrini@hotmail.it>
2014-07-13 15:43:22 +0100

    Added SFSwiftNotification class

---
+++ Classes/SFSwiftNotification.swift
@@ -0,0 +1,27 @@
+//
+//  SFSwiftNotification.swift
+//  SFSwiftNotification
+//
+//  Created by Simone Ferrini on 13/07/14.
+//  Copyright (c) 2014 sferrini. All rights reserved.
+//
+
+import UIKit
+
+class SFSwiftNotification: UIView {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        // Initialization code
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
